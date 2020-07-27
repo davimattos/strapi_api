db2abf18f35813d2b53eed5a16adafd5e16e84b2
Davi Mattos <davimattosdev@gmail.com>
2020-07-26 21:31:34 -0300

    refactor: setando a lib do ckEditor

--- app/plugins/wysiwyg/admin/src/index.js
+++ app/plugins/wysiwyg/admin/src/index.js
@@ -1,52 +1,29 @@
-import pluginPkg from '../../package.json';
-import pluginId from './pluginId';
-import App from './containers/App';
-import Initializer from './containers/Initializer';
-import lifecycles from './lifecycles';
-import trads from './translations';
+import pluginPkg from "../../package.json";
+import Wysiwyg from "./components/Wysiwyg";
+import pluginId from "./pluginId";
 
-export default strapi => {
-  const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
-  const icon = pluginPkg.strapi.icon;
-  const name = pluginPkg.strapi.name;
+export default (strapi) => {
+  const pluginDescription =
+    pluginPkg.strapi.description || pluginPkg.description;
 
   const plugin = {
     blockerComponent: null,
     blockerComponentProps: {},
     description: pluginDescription,
-    icon,
+    icon: pluginPkg.strapi.icon,
     id: pluginId,
-    initializer: Initializer,
+    initializer: () => null,
     injectedComponents: [],
-    isReady: false,
+    isReady: true,
     isRequired: pluginPkg.strapi.required || false,
-    layout: null,
-    lifecycles,
-    mainComponent: App,
-    name,
+    mainComponent: null,
+    name: pluginPkg.strapi.name,
     preventComponentRendering: false,
-    trads,
-    menu: {
-      pluginsSection: [
-        {
-          destination: `/plugins/${pluginId}`,
-          icon,
-          label: {
-            id: `${pluginId}.plugin.name`,
-            defaultMessage: name,
-          },
-          name,
-          permissions: [
-            // Uncomment to set the permissions of the plugin here
-            // {
-            //   action: '', // the action name should be plugins::plugin-name.actionType
-            //   subject: null,
-            // },
-          ],
-        },
-      ],
-    },
+    settings: null,
+    trads: {},
   };
+
+  strapi.registerField({ type: "wysiwyg", Component: Wysiwyg });
 
   return strapi.registerPlugin(plugin);
 };
